a8dd87eeae1589fc231bc096791e6db3df01017e
rise <rladntjd>
2021-02-21 16:40:17 +0900

    0221

--- src/image_pub_cpp.cpp
+++ src/image_pub_cpp.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <cstdlib>
 #include <math.h>
+#include <string>
 #include <ctime>
 #include <sstream>
 #include <image_transport/image_transport.h>
@@ -13,7 +14,6 @@
 
 int probability;
 int trans_rand;
-double average ;
 double standardDev; 
 double result ;
 
@@ -23,9 +23,9 @@ struct parameter{
   float power[2];
 };
 struct parameter seg_to_constant[5];
-static uint8_t color[5][3] = {{26, 27, 45},{70, 76, 194},{6, 206, 29},{152, 67, 200},{1, 60, 44}};
-static float constant[5][2] = {{0.001106, 0.05335}, {0.001507, 0.0843}, {0.03485, 0.1851}, {0.02108, 0.1435}, {0.001552, 0.00654}};
-static float power[5][2] = {{1.966, 0.2968}, {1.526, 0.3572}, {1.497, 0.2523}, {1.9, 0.3429}, {0.997, 0.4292}};
+static uint8_t color[5][3] = {{26, 27, 45},{70, 76, 194},{152, 67, 200},{6, 206, 29},{1, 60, 44}};
+static float constant[5][2] = {{0.001106, 0.05335}, {0.001507, 0.0843}, {0.03485, 0.1851}, {0.001802, 0.01455}, {0.001552, 0.00654}};
+static float power[5][2] = {{1.966, 0.2968}, {1.526, 0.3572}, {1.497, 0.2523}, {1.245, 0.4688}, {0.997, 0.4292}};
 static int trans_prob;
 
 float max(float a, float b){
@@ -55,8 +55,8 @@ class ImageConverter
   image_transport::Subscriber image_sub_seg_;
   image_transport::Publisher image_pub_;
   cv_bridge::CvImagePtr Segmentation_ptr;
-  cv_bridge::CvImagePtr img_old_ptr;
-  cv_bridge::CvImagePtr img_original_ptr;
+  cv::Mat img_original_ptr;
+  cv::Mat img_old_ptr;
 public:
   ImageConverter()
     : it_(nh_)
@@ -70,10 +70,12 @@ public:
   {
     cv_bridge::CvImagePtr cv_ptr;
     sensor_msgs::ImagePtr message;
-
+    float deviation = 0; 
+    float average ;
+    float value_D = 0.35; // 0.3~0.5까지 선택
+    float value_C = 0.03796 / exp(value_D * 3);
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
-    img_original_ptr = cv_ptr;
-    //cv::Mat int_image(480,640,CV_16UC1);
+    img_original_ptr = cv_ptr->image.clone();
 
     if(Segmentation_ptr == NULL){
       return;
@@ -87,33 +89,37 @@ public:
         uchar g = Segmentation_ptr->image.at<cv::Vec3b>(i,j)[1];
         uchar r = Segmentation_ptr->image.at<cv::Vec3b>(i,j)[2];
 
-        if(cv_ptr->image.at<float>(i,j) > 13.) {
+        if(cv_ptr->image.at<float>(i,j) > 13. || probability < 10000*0.01) {
           cv_ptr->image.at<float>(i,j) = 0;
-        }
-        else if(probability < 10000*0.01) { // fillout error
-          cv_ptr->image.at<float>(i,j) = 0;
+          img_original_ptr.at<float>(i,j) = 0;
         }
         else{
 
-          // if(img_old_ptr == NULL) {
-          //   cv_ptr->image.at<float>(i,j) = img_original_ptr->image.at<float>(i,j);
-          // }
-          // else if(trans_rand < 9000) {
-          //   cv_ptr->image.at<float>(i,j) = max(img_old_ptr->image.at<float>(i,j), img_original_ptr->image.at<float>(i,j));
-          // }
-          // else{
-          //   cv_ptr->image.at<float>(i,j) = min(img_old_ptr->image.at<float>(i,j), img_original_ptr->image.at<float>(i,j));
-          // }
+          if(img_old_ptr.empty()) {
+            cv_ptr->image.at<float>(i,j) = img_original_ptr.at<float>(i,j);
+          }
+          else if(trans_rand < 5000) {
+            cv_ptr->image.at<float>(i,j) = max(img_old_ptr.at<float>(i,j), img_original_ptr.at<float>(i,j));
+          }
+          else{
+            cv_ptr->image.at<float>(i,j) = min(img_old_ptr.at<float>(i,j), img_original_ptr.at<float>(i,j));
+          }
 
           for(int k=0; k<5; k++){
             if(b == seg_to_constant[k].color[0] && g == seg_to_constant[k].color[1] && r == seg_to_constant[k].color[2]) {
               average = cv_ptr->image.at<float>(i,j);
               //printf("%f\n", average);
               if(average < 3.){
-                cv_ptr->image.at<float>(i,j) = (float) gaussianRandom(average, seg_to_constant[k].constant[0] * exp(seg_to_constant[k].power[0] * average));
+                cv_ptr->image.at<float>(i,j) =  gaussianRandom(average, 0.0014 * exp(1.1 * average));
+                deviation = 0.0014 * exp(1.1 * average);
+                //cv_ptr->image.at<float>(i,j) =  gaussianRandom(average, seg_to_constant[k].constant[0] * exp(seg_to_constant[k].power[0]));
+                //deviation = seg_to_constant[k].constant[0] * exp(seg_to_constant[k].power[0] * average);
               }
               else {
-                cv_ptr->image.at<float>(i,j) = (float) gaussianRandom(average, seg_to_constant[k].constant[1] * exp(seg_to_constant[k].power[1] * average));
+                cv_ptr->image.at<float>(i,j) = gaussianRandom(average, value_C * exp(value_D * average));
+                deviation = value_C * exp(value_D * average);
+                //cv_ptr->image.at<float>(i,j) =  gaussianRandom(average, seg_to_constant[k].constant[1] * exp(seg_to_constant[k].power[1]));
+                //deviation = seg_to_constant[k].constant[1] * exp(seg_to_constant[k].power[1] * average);
               }
               break;
             }
@@ -123,11 +129,15 @@ public:
         //cv_ptr->image.at<_Float32>(i,j) = (_Float32) 1./1000. * int_image.at<uint16_t>(i,j);
       }
     }
-    printf("%f\n", cv_ptr->image.at<float>(240,320));
+    printf("ori_dist : %f\n", cv_ptr->image.at<float>(240,320));
+    printf("deviation : %f\n", deviation);
+    if(!img_old_ptr.empty()){
+     printf("old_dist : %f\n", img_old_ptr.at<float>(240,320));
+    }
     // Output modified video stream
     //printf("%f\n", cv_ptr->image.at<float>(100,600));
     // message = cv_bridge::CvImage(std_msgs::Header(), "32FC1", int_image).toImageMsg();
-    img_old_ptr = img_original_ptr;
+    img_old_ptr = img_original_ptr.clone();
     image_pub_.publish(cv_ptr->toImageMsg());
   }
   
---
+++ src/min_max_data.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+import rospy
+import cv2 as cv
+import sys
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int32
+from cv_bridge import CvBridge
+from math import sin, cos, sqrt, pi
+import numpy as np
+
+def check(item):
+    if item >= 11000:
+        return True
+    else :
+        return False
+
+class Mission:
+    def __init__(self):
+        # Subscriber
+        rospy.Subscriber('/camera/depth/image_rect_raw', Image, self.depthCb)
+        self.depth = np.empty((480,640), dtype = np.uint16)
+        self.bridge = CvBridge()
+        self.f = open('in_pl_1_50_filtered_max_data.txt', 'w')
+        self.f1 = open('in_pl_1_50_filtered_min_data.txt', 'w')
+        self.test_pub = rospy.Publisher('/testing_frequency', Int32, queue_size=10000)
+        self.step = 1
+        self.process = {0:[319,213,346,242,1], # 1 material 50~6
+                        1:[330,215,351,240,1], # 2 material 50~6
+                        2:[329,216,350,234,1], #3 material 50~6
+                        3:[332,210,358,246,1], #4 material 50~6
+                        4:[241,135,379,307,5], #1 material 1
+                        5:[152,58,409,367,10], #1 material 50
+                        6:[246,133,380,301,5], #2 material 1
+                        7:[171,47,397,366,10], #2 material 50
+                        8:[247,140,384,298,5], #3 material 1
+                        9:[171,57,429,354,10], #3 material 50
+                        10:[242,137,365,299,5], #4 material 1
+                        11:[174,62,403,349,10]}.get(self.step) #4 material 50
+        print(self.process)
+        self.width_s = self.process[0] #has to be changed by depth image
+        print(self.width_s)
+        self.width_e = self.process[2] #has to be changed by depth image
+        self.height_s = self.process[1]
+        self.height_e = self.process[3]
+        self.div_num = self.process[4]
+        self.count = 0
+        for i in range((self.width_e -  self.width_s)/self.div_num):
+            for j in range((self.height_e - self.height_s)/self.div_num):
+                s2 = 'self.pixel_data_%d_%d = []' %(i*self.div_num,j*self.div_num)  # x, y coordinate
+                exec(s2)
+    def depthCb(self, msg):
+        depth = self.bridge.imgmsg_to_cv2(msg, "16UC1")
+        test_msg = Int32()
+        test_msg.data = 1
+        mean_data = []
+        self.depth = np.array(depth)
+        self.depth = (self.depth.astype(np.int64)) 
+        
+        for i in range((self.width_e -  self.width_s)/self.div_num):
+            for j in range((self.height_e - self.height_s)/self.div_num):
+                if (self.depth[j*self.div_num + self.height_s,i*self.div_num + self.width_s] != 0):
+                    s3 = 'self.pixel_data_%d_%d.append(self.depth[j*self.div_num + self.height_s,i*self.div_num + self.width_s])' %(i*self.div_num,j*self.div_num)  
+                    exec(s3)
+
+    def main(self):
+        
+        pass
+
+    def final(self):
+        pixel_max_data = ""
+        pixel_min_data = ""
+        for i in range((self.width_e -  self.width_s)/self.div_num):
+            for j in range((self.height_e - self.height_s)/self.div_num):
+                s1 = 'pixel_max_data += str(max(self.pixel_data_%d_%d))' %(i*self.div_num,j*self.div_num)
+                exec(s1)
+                s2 = 'pixel_max_data += str(max(self.pixel_data_%d_%d))' %(i*self.div_num,j*self.div_num)
+                exec(s2)
+                pixel_max_data += '\t'
+                pixel_min_data += '\t'
+            pixel_max_data += "\n"
+            pixel_min_data += '\t'
+            
+        self.f.write(pixel_max_data)
+        self.f1.write(pixel_min_data)
+        
+        self.f.close()
+        self.f1.close()
+        
+
+if __name__ == '__main__':
+    # Initialize node
+    rospy.init_node('main', anonymous=True, disable_signals=True)
+    mission = Mission()
+
+    rate = rospy.Rate(20.0)
+
+    try:
+
+        while not rospy.is_shutdown():
+            mission.main()
+            rate.sleep()
+
+    except rospy.ROSInterruptException:
+        pass
+    finally :
+        mission.final()
